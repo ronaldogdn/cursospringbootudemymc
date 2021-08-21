12e4d97b07a48baff7e954138fd898b1953c8746
Ronaldo Gomes do Nascimento <ronaldogdn@gmail.com>
2021-08-20 21:27:21 -0300

    projeto criado

---
+++ src/main/java/com/ronaldo/curso/CursoSpringBootUdemyApplication.java
@@ -0,0 +1,13 @@
+package com.ronaldo.curso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoSpringBootUdemyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoSpringBootUdemyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ronaldo/curso/CursoSpringBootUdemyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ronaldo.curso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoSpringBootUdemyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
