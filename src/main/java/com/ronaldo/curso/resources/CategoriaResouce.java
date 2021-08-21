68d3c6b90f11f3792d516e2713aef80bb31be55c
Ronaldo Gomes do Nascimento <ronaldogdn@gmail.com>
2021-08-21 18:27:24 -0300

    Testando a primeira classe de Domínio - Categoria

--- src/main/java/com/ronaldo/curso/resources/CategoriaResouce.java
+++ src/main/java/com/ronaldo/curso/resources/CategoriaResouce.java
@@ -1,19 +1,31 @@
 package com.ronaldo.curso.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.ronaldo.curso.domain.Categoria;
 
 /**
  * Classe para fazer o Controller da api
  * @author ronaldo
  */
 @RestController
-@RequestMapping(value = "categorias")
+@RequestMapping(value = "/categorias")
 public class CategoriaResouce {
 
 	@GetMapping
-	public String listar() {
-		return "Rest está funcionando";
+	public List<Categoria> listar() {
+		Categoria cat1 = new Categoria(1,"informática");
+		Categoria cat2 = new Categoria(1,"informática");
+
+		List<Categoria> lista = new ArrayList<>();
+		lista.add(cat1);
+		lista.add(cat2);
+		
+		return lista;
 	}
 }
