31e33707830af8e0649c80e3bde0b78e17074f82
Ronaldo Gomes do Nascimento <ronaldogdn@gmail.com>
2021-08-22 20:57:01 -0300

    Produto e associação muitos para muitos

--- src/main/java/com/ronaldo/curso/CursoSpringBootUdemyApplication.java
+++ src/main/java/com/ronaldo/curso/CursoSpringBootUdemyApplication.java
@@ -6,17 +6,19 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.h2.H2ConsoleAutoConfiguration;
 
 import com.ronaldo.curso.domain.Categoria;
+import com.ronaldo.curso.domain.Produto;
 import com.ronaldo.curso.repositories.CategoriaRepository;
+import com.ronaldo.curso.repositories.ProdutoRepository;
 
 @SpringBootApplication
 public class CursoSpringBootUdemyApplication implements CommandLineRunner{
 
 	@Autowired
 	private CategoriaRepository categoriaRepository;
-	
+	@Autowired
+	private ProdutoRepository produtoRepository;
 	public static void main(String[] args) {
 		SpringApplication.run(CursoSpringBootUdemyApplication.class, args);
 	}
@@ -28,7 +30,19 @@ public class CursoSpringBootUdemyApplication implements CommandLineRunner{
 		Categoria cat1 = new Categoria(null,"Informática");
 		Categoria cat2 = new Categoria(null,"Escritório");
 		
+		Produto p1 = new Produto(null,"computador",2000.00);
+		Produto p2 = new Produto(null,"impressora",800.00);
+		Produto p3 = new Produto(null,"mouse",80.00);
+		
+		cat1.getProdutos().addAll(Arrays.asList(p1,p2,p3));
+		cat2.getProdutos().addAll(Arrays.asList(p2));
+		
+		p1.getCategorias().addAll(Arrays.asList(cat1));
+		p2.getCategorias().addAll(Arrays.asList(cat1,cat2));
+		p3.getCategorias().addAll(Arrays.asList(cat1));
+		
 		categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
+		produtoRepository.saveAll(Arrays.asList(p1,p2,p3));
 	}
 
 }
---
+++ src/main/java/com/ronaldo/curso/repositories/ProdutoRepository.java
@@ -0,0 +1,11 @@
+package com.ronaldo.curso.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.ronaldo.curso.domain.Produto;
+
+@Repository
+public interface ProdutoRepository extends JpaRepository<Produto, Integer>{
+
+}
