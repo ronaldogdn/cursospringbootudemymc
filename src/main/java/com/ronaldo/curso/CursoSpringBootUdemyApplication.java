697143a11264656198e33f4c6d1bbdb696cbef50
Ronaldo Gomes do Nascimento <ronaldogdn@gmail.com>
2021-08-21 21:29:37 -0300

    criando operação de instanciação

--- src/main/java/com/ronaldo/curso/CursoSpringBootUdemyApplication.java
+++ src/main/java/com/ronaldo/curso/CursoSpringBootUdemyApplication.java
@@ -1,13 +1,34 @@
 package com.ronaldo.curso;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.h2.H2ConsoleAutoConfiguration;
+
+import com.ronaldo.curso.domain.Categoria;
+import com.ronaldo.curso.repositories.CategoriaRepository;
 
 @SpringBootApplication
-public class CursoSpringBootUdemyApplication {
+public class CursoSpringBootUdemyApplication implements CommandLineRunner{
 
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursoSpringBootUdemyApplication.class, args);
+	}
+	/**
+	 * Método para inserir automáticamente no h2 em memória ram
+	 */
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria(null,"Informática");
+		Categoria cat2 = new Categoria(null,"Escritório");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
 	}
 
 }
