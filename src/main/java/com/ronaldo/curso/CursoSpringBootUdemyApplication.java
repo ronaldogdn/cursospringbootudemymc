3cccd41a97ccbdad8eed094630b6cc86dee8f269
Ronaldo Gomes do Nascimento <ronaldogdn@gmail.com>
2021-08-23 15:22:20 -0300

    Estado e Cidade Entity

--- src/main/java/com/ronaldo/curso/CursoSpringBootUdemyApplication.java
+++ src/main/java/com/ronaldo/curso/CursoSpringBootUdemyApplication.java
@@ -8,8 +8,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.ronaldo.curso.domain.Categoria;
+import com.ronaldo.curso.domain.Cidade;
+import com.ronaldo.curso.domain.Estado;
 import com.ronaldo.curso.domain.Produto;
 import com.ronaldo.curso.repositories.CategoriaRepository;
+import com.ronaldo.curso.repositories.CidadeRepository;
+import com.ronaldo.curso.repositories.EstadoRepository;
 import com.ronaldo.curso.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -19,6 +23,13 @@ public class CursoSpringBootUdemyApplication implements CommandLineRunner{
 	private CategoriaRepository categoriaRepository;
 	@Autowired
 	private ProdutoRepository produtoRepository;
+	@Autowired
+	private CidadeRepository cidadeRepository;
+	@Autowired
+	private EstadoRepository estadoRepository;
+	
+	
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursoSpringBootUdemyApplication.class, args);
 	}
@@ -43,6 +54,19 @@ public class CursoSpringBootUdemyApplication implements CommandLineRunner{
 		
 		categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
 		produtoRepository.saveAll(Arrays.asList(p1,p2,p3));
+		
+		Estado est1 = new Estado(null,"Minas Gerais");
+		Estado est2 = new Estado(null,"São Paulo");
+		
+		Cidade c1 = new Cidade(null,"Uberlândia",est1);
+		Cidade c2 = new Cidade(null,"São Paulo",est2);
+		Cidade c3 = new Cidade(null,"Campinas",est2);
+		
+		est1.getCidades().addAll(Arrays.asList(c1));
+		est2.getCidades().addAll(Arrays.asList(c2,c3));
+		
+		estadoRepository.saveAll(Arrays.asList(est1,est2));
+		cidadeRepository.saveAll(Arrays.asList(c1,c2,c3));
 	}
 
 }
