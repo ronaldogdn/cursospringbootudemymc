3cf0f5af1efb8997b590e4ddce77c4846d385d97
Ronaldo Gomes do Nascimento <ronaldogdn@gmail.com>
2021-08-26 21:43:36 -0300

    Deletando uma categoria com DELETE

--- src/main/java/com/ronaldo/curso/resources/exception/ResourceExceptionHandler.java
+++ src/main/java/com/ronaldo/curso/resources/exception/ResourceExceptionHandler.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.ronaldo.curso.services.exception.DataIntegrityException;
 import com.ronaldo.curso.services.exception.ObjectNotFoundException;
 
 @ControllerAdvice//tratamento global de erros
@@ -17,5 +18,11 @@ public class ResourceExceptionHandler {
 		StandartError err = new StandartError(HttpStatus.NOT_FOUND.value(), exception.getMessage(), System.currentTimeMillis());
 		
 		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(err);
+	}
+	@ExceptionHandler(DataIntegrityException.class)
+	public ResponseEntity<StandartError> dataIntegrity(DataIntegrityException exception, HttpServletRequest request){
+		StandartError err = new StandartError(HttpStatus.BAD_REQUEST.value(), exception.getMessage(), System.currentTimeMillis());
+		
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
 	}
 }
--- src/main/java/com/ronaldo/curso/services/CategoriaService.java
+++ src/main/java/com/ronaldo/curso/services/CategoriaService.java
@@ -3,10 +3,12 @@ package com.ronaldo.curso.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.ronaldo.curso.domain.Categoria;
 import com.ronaldo.curso.repositories.CategoriaRepository;
+import com.ronaldo.curso.services.exception.DataIntegrityException;
 import com.ronaldo.curso.services.exception.ObjectNotFoundException;
 
 @Service
@@ -34,5 +36,14 @@ public class CategoriaService {
 	public Categoria update(Categoria obj) {
 		find(obj.getId());
 		return repo.save(obj);
+	}
+	
+	public void delete(Integer id) {
+		find(id);
+		try {
+			repo.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("não é possível excluir uma categoria que tem produtos");
+		}
 	}
 }
