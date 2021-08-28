930482808eb584a4ca2ea1ebaee08e8db4068c16
Ronaldo Gomes do Nascimento <ronaldogdn@gmail.com>
2021-08-27 22:13:00 -0300

    Paginação com parâmetros na requisição

--- src/main/java/com/ronaldo/curso/services/CategoriaService.java
+++ src/main/java/com/ronaldo/curso/services/CategoriaService.java
@@ -5,6 +5,9 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.ronaldo.curso.domain.Categoria;
@@ -50,5 +53,33 @@ public class CategoriaService {
 
 	public List<Categoria> findAll() {
 		return repo.findAll();
+	}
+	/**
+	 * 
+	 * @param page Contagem de página: 0,1,2...
+	 * @param linesPerPage Quantas linhas por página eu quero
+	 * @param orderBy Campo a ser ordenado
+	 * @param direction Ordenação ASC ou DESC
+	 * @return
+	 */
+	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
+		boolean error = true;
+		if(page < 0 || linesPerPage < 0) {
+			throw new DataIntegrityException("não é possível mostrar as categorias");
+		}
+		if(orderBy.equals("id") || orderBy.equals("nome")) {
+			error = false;
+		}
+		
+		if(error) {
+			throw new DataIntegrityException("não é possível mostrar as categorias");
+		}
+		try {
+			//para spring versão 2 ou superior
+			PageRequest pageRequest = PageRequest.of(page,linesPerPage, Direction.valueOf(direction),orderBy);
+			return repo.findAll(pageRequest);
+		} catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("não é possível mostrar as categorias");
+		}
 	}
 }
