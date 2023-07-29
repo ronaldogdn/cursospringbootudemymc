ff4ec9e947843c3a0f8d10e0a751da5924c89bdc
Ronaldo Gomes do Nascimento <ronaldogdn@gmail.com>
2023-07-28 21:39:29 -0300

    retirado @Repositorie na versão atual

--- src/main/java/com/ronaldo/curso/repositories/PedidoRepository.java
+++ src/main/java/com/ronaldo/curso/repositories/PedidoRepository.java
@@ -1,11 +1,10 @@
 package com.ronaldo.curso.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import com.ronaldo.curso.domain.Pedido;
 
-@Repository
+//@Repository => nas versões 3.1 ou superiro não é mais necessário
 public interface PedidoRepository extends JpaRepository<Pedido, Integer>{
 
 }
