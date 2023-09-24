40167c0d9f984b697f8e966e34df0ec9ed477aff
Ronaldo Gomes do Nascimento <ronaldogdn@gmail.com>
2023-09-23 21:19:11 -0300

    add comment, removed unnecessary line

--- src/main/java/com/ronaldo/curso/repositories/ProdutoRepository.java
+++ src/main/java/com/ronaldo/curso/repositories/ProdutoRepository.java
@@ -5,7 +5,6 @@ import java.util.List;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 //import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -14,11 +13,12 @@ import com.ronaldo.curso.domain.Produto;
 
 //@Repository //atualização: não é mais necessário @Repository nas versões atuais
 public interface ProdutoRepository extends JpaRepository<Produto, Integer>{
-
+	/**
+	 * Pode ser usada a QUERY diretamente ou find do JPA
+	 */
 //	@Transactional(readOnly=true)
 //	@Query("SELECT DISTINCT obj FROM Produto obj INNER JOIN obj.categorias cat WHERE obj.nome LIKE %:nome% AND cat IN :categorias")
 //	Page<Produto> search(@Param("nome") String nome, @Param("categorias") List<Categoria> categorias, Pageable pageRequest);
 	@Transactional(readOnly=true)
-	@Query("SELECT DISTINCT obj FROM Produto obj INNER JOIN obj.categorias cat WHERE obj.nome LIKE %:nome% AND cat IN :categorias")
 	Page<Produto> findDistincByNomeContainingAndCategoriasIn(String nome, List<Categoria> categorias,Pageable pageRequest);
 }
